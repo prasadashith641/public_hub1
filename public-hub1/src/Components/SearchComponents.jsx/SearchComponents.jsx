19dddab0914b634d4068c58551b3d4861c5a850f
prasadashith641 <142388504+prasadashith641@users.noreply.github.com>
2023-09-19 10:23:21 +0530

    updated  then error page unresponsive  due to
    updated  then error page unresponsive  due to the recursive function

--- public-hub1/src/Components/SearchComponents.jsx/SearchComponents.jsx
+++ public-hub1/src/Components/SearchComponents.jsx/SearchComponents.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import "./SearchComponents.css"
+import React from "react";
+import "./SearchComponents.css";
+import SearchUserCard from "./SearchUserCard";
 
 const SearchComponents = () => {
   return (
-    <div className='searchContainer'>
-        <div className='px-3 pb-5'>
-            <h1 className='text-x1 pb-5'>Search</h1>
-            <input className='searchInput' type='text' placeholder='Search...' />
-        </div>
+    <div className="searchContainer">
+      <div className="px-3 pb-5">
+        <h1 className="text-x1 pb-5">Search</h1>
+        <input className="searchInput" type="text" placeholder="Search..." />
+      </div>
 
-        <hr />
+      <hr />
 
-        <div className='px-3 pt-5'>
-          {[1,1,1,1,1,1].map((item)=><SearchComponents/>)}
-          
-        </div>
+      <div className="px-3 pt-5">
+        {[1, 1, 1, 1, 1, 1].map((item) => (
+          <SearchUserCard />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchComponents
+export default SearchComponents;
--- public-hub1/src/Components/Sidebar/SidebarConfig.js
+++ public-hub1/src/Components/Sidebar/SidebarConfig.js
@@ -10,18 +10,15 @@ import {
   AiOutlineMessage,
   AiOutlinePlusCircle,
   AiOutlineSearch,
- 
-  
 } from "react-icons/ai";
 import { RiVideoFill, RiVideoLine } from "react-icons/ri";
- import { CgProfile } from "react-icons/cg";
-
+import { CgProfile } from "react-icons/cg";
 
 export const mainu = [
   {
     title: "Home",
     icon: <AiOutlineHome className="text-2xl mr-5"></AiOutlineHome>,
-    iactiveIcon: <AiFillHome className="text-2xl mr-5"></AiFillHome>,
+    activeIcon: <AiFillHome className="text-2xl mr-5"></AiFillHome>,
   },
   {
     title: "Search",
@@ -43,20 +40,20 @@ export const mainu = [
     icon: <AiOutlineMessage className="text-2xl mr-5"></AiOutlineMessage>,
     activeIcon: <AiFillMessage className="text-2xl mr-5"></AiFillMessage>,
   },
- {
+  {
     title: "Create",
     icon: <AiOutlinePlusCircle className="text-2xl mr-5"></AiOutlinePlusCircle>,
     activeIcon: <AiFillPlusCircle className="text-2xl mr-5"></AiFillPlusCircle>,
-  },  
-  
-   {
+  },
+
+  {
     title: "Notification",
     icon: <AiOutlineHeart className="text-2xl mr-5"></AiOutlineHeart>,
-    iactiveIcon: <AiFillHeart className="text-2xl mr-5"></AiFillHeart>,
+    activeIcon: <AiFillHeart className="text-2xl mr-5"></AiFillHeart>,
   },
   {
     title: "Profile",
-    icons: <CgProfile className="text-2xl mr-5"></CgProfile>,
+    icon: <CgProfile className="text-2xl mr-5"></CgProfile>,
     activeIcon: <CgProfile className="text-2xl mr-5"></CgProfile>,
   },
 ];
